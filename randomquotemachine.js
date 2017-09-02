0d2a6e3cfe59860ef4b23ce995348f3e3a978d3e
benjaminelliott <benjamin.elliott.be@gmail.com>
2017-09-02 18:15:48 -0400

    Create randomquotemachine.js

---
+++ randomquotemachine.js
@@ -0,0 +1,10 @@
+var quotes = {
+  "quote1" : {
+    "author" : "Stacy Jones",
+    "text" : "'Ben is the best, I love him so much'"
+  },
+  "quote2" : {
+    "author" : "Benjamin Elliott",
+    "text" : "'Stacy is alright, I guess'"
+  }
+}
